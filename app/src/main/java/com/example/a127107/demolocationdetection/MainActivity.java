1eb8c62b8f9145abf30f924c4436d1ab866045f8
tzejian <akernus.lr@gmail.com>
2017-07-18 10:39:44 +0800

    final

---
+++ app/src/main/java/com/example/a127107/demolocationdetection/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.a127107.demolocationdetection;
+
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLocation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED
+                ||  permissionCheck_Fine  == PermissionChecker.PERMISSION_GRANTED){
+            mLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+
+            LocationRequest mLocationRequest = LocationRequest.create();
+            mLocationRequest.setPriority(LocationRequest
+                    .PRIORITY_BALANCED_POWER_ACCURACY);
+            mLocationRequest.setInterval(10000);
+            mLocationRequest.setFastestInterval(5000);
+            mLocationRequest.setSmallestDisplacement(100);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                    mLocationRequest, this);
+
+        } else {
+            mLocation = null;
+            Toast.makeText(MainActivity.this,
+                    "Permission not granted to retrieve location info",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        if (mLocation != null) {
+            Toast.makeText(this, "Lat : " + mLocation.getLatitude() +
+                            " Lng : " + mLocation.getLongitude(),
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Location not Detected",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {}
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        //the detected location is given by the variable location in the signature
+
+        Toast.makeText(this, "Lat : " + location.getLatitude() + " Lng : " +
+                location.getLongitude(), Toast.LENGTH_SHORT).show();
+    }
+
+
+}
